be8ae065edc636f1b014ad94a97da846ab9effc0
Majid Abolfathi <107215137+Majiiddddd@users.noreply.github.com>
2023-07-19 09:18:33 +0200

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,18 +1,4 @@
-// let alltest = document.querySelectorById("#alltest");
-// let truetest = document.querySelectorById("#True");
-// let falsetest = document.querySelectorById("#False");
-// let button = document.querySelector('button');
-// let p = document.querySelectorById(#message);
 
-// button.addEventListener(onclick , percentage);
-// function percentage(){
-//     if((truetest.value + falsetest.value) >= alltest.value){
-//         (100*truetest.value)/alltest.value;
-//     }
-//     else(){
-//         p.innerHTML = `undefined value`
-//     }
-// }
 
 let alltest = document.getElementById("alltest");
 let truetest = document.getElementById("True");
